7322206b78dece10a5470efdb73833e14daec3b1
serge.vakulenko <serge.vakulenko@67cc22b9-0949-0410-9f4c-0712e9df7174>
2009-09-20 19:29:47 +0000

    SIMH: example1.m20 is working fine.

--- trunk/simh/m20_defs.h
+++ trunk/simh/m20_defs.h
@@ -24,6 +24,26 @@ enum {
 	STOP_ADDOVF,				/* addition overflow */
 	STOP_EXPOVF,				/* exponent overflow */
 	STOP_MULOVF,				/* multiplication overflow */
+	STOP_DIVOVF,				/* division overflow */
+	STOP_DIVMOVF,				/* division mantissa overflow */
+	STOP_NEGSQRT,				/* division mantissa overflow */
+	STOP_SQRTERR,				/* sqrt error */
+	STOP_READERR,				/* drum read error */
+	STOP_BADRLEN,				/* invalid drum read length */
+	STOP_BADWLEN,				/* invalid drum write length */
+	STOP_WRERR,				/* drum write error error */
+	STOP_DRUMINVAL,				/* invalid drum control word */
+	STOP_DRUMINVDATA,			/* reading uninialized drum data */
+	STOP_TAPEINVAL,				/* invalid tape control word */
+	STOP_TAPEFMTINVAL,			/* invalid tape format word */
+	STOP_TAPEUNSUPP,			/* tape not implemented */
+	STOP_TAPEFMTUNSUPP,			/* tape formatting not implemented */
+	STOP_PUNCHUNSUPP,			/* punch not implemented */
+	STOP_RPUNCHUNSUPP,			/* punch reader not implemented */
+	STOP_EXTINVAL,				/* invalid control word */
+	STOP_INVARG,				/* invalid argument of instruction */
+	STOP_ASSERT,				/* assertion failed */
+	STOP_MBINVAL,				/* MB command without MA */
 };
 
 /*
